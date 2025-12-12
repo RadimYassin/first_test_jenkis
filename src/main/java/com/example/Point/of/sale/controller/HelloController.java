0a911db1a37576f1d530325aa355dbb5f7ccd276
RadimYassin <radimyassin20@gmail.com>
2025-12-12 23:02:05 +0100

    chnages

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -15,12 +15,12 @@ public class HelloController {
 
     @GetMapping("/user")
     public String getUsers(){
-        return "User sjsjs";
+        return "User dsjhqwjdjqwdc sjsjs";
     }
 
 
     @GetMapping("/presentation")
     public String getPresentation(){
-        return "presentation  test ";
+        return "presentation  hsjsj test ";
     }
 }
